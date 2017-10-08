38faa3070a98ad90e5f5585a56a290a94a967bd0
Jeremy Chen <jeremy886@gmail.com>
2017-10-08 22:16:38 +1100

    blog v2

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sukmhyq8sq*x+=a9op3ao&e=s9-hz-6klp(ljn@g3#=%quqtm5'
+
+SECRET_KEY = open(os.path.expanduser('~/.djangogirls-secret')).read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
